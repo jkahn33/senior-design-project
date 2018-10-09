2752c2b8824f2067fe747ef79d32fa9ec43fd3aa
jkahn33 <jgkahn33@gmail.com>
2018-10-09 12:51:35 -0400

    removed view files and made initial method return test JSON data

--- src/main/java/senior/design/group10/controller/WindowsController.java
+++ src/main/java/senior/design/group10/controller/WindowsController.java
@@ -2,6 +2,8 @@ package senior.design.group10.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import senior.design.group10.objects.TestWrap;
 
 import java.util.logging.Logger;
 
@@ -11,7 +13,8 @@ public class WindowsController {
     private final static Logger log = Logger.getLogger(WindowsController.class.getName());
 
     @GetMapping("/")
-    public String homePage(){
-        return "home";
+    @ResponseBody
+    public TestWrap homePage(){
+        return new TestWrap("Hello, world!");
     }
 }
