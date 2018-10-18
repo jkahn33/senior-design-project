5b50f95c1f148645ca2a9aad578f7d34efed0c51
jkahn33 <jgkahn33@gmail.com>
2018-10-18 16:17:00 -0400

    removed test

--- src/main/java/senior/design/group10/objects/PrinterReservations.java
+++ src/main/java/senior/design/group10/objects/PrinterReservations.java
@@ -4,7 +4,7 @@ import java.sql.Timestamp;
 
 public class PrinterReservations
 {
-	private String username1;
+	private String username;
 	private String fiveDigExt;
 	private String jobDescription;
 	private String jobDuration;
