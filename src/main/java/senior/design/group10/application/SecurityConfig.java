3e49931f8af5d651229799c568a8cebfa244bced
jkahn33 <jgkahn33@gmail.com>
2018-10-31 11:40:43 -0400

    made security ignore windows endpoints as well

--- src/main/java/senior/design/group10/application/SecurityConfig.java
+++ src/main/java/senior/design/group10/application/SecurityConfig.java
@@ -18,6 +18,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/android/**");
+        web.ignoring().antMatchers("/android/**", "/windows/**");
     }
 }
